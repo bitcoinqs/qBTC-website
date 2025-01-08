cc1f2846c5e21e5f564b69570fcbcd54a4ab896f
Christian Papathanasiou <cp@gcx.io>
2025-01-08 14:17:08 +0200

    Update DocsPage.tsx

--- src/pages/DocsPage.tsx
+++ src/pages/DocsPage.tsx
@@ -2,5 +2,11 @@ import React from 'react';
 import Docs from '../components/Docs';
 
 export default function DocsPage() {
-  return <Docs />;
+  return (
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-grow">
+        <Docs />
+      </div>
+    </div>
+  );
 }
