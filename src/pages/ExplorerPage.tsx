37cce0c9c208a83373956e4c1a1fe42552c938db
Christian Papathanasiou <cp@gcx.io>
2025-01-19 21:22:15 +0200

    Update ExplorerPage.tsx

--- src/pages/ExplorerPage.tsx
+++ src/pages/ExplorerPage.tsx
@@ -317,7 +317,6 @@ export default function ExplorerPage() {
                     <>
                       <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Merkle Root</th>
                       <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Bitcoin TX</th>
-                      <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Transactions</th>
                       <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Time</th>
                       <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
                     </>
