cf6e263913ba4317f6dcb551952fe339f3a47f28
Christian Papathanasiou <cp@gcx.io>
2025-03-11 19:50:49 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -21,7 +21,7 @@ export default function Footer() {
         </div>
         <div className="mt-8 md:mt-0 md:order-1">
           <p className="text-center text-base text-gray-400">
-            &copy; 2025 Bitcoin QS / Quantum Safe Technologies Corp Delaware. All rights reserved.
+            &copy; 2025 Bitcoin QS All rights reserved.
           </p>
         </div>
       </div>
