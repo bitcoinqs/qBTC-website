eae5f1a96534138827ac2ef8b67bce129ab3dacf
Christian Papathanasiou <cp@gcx.io>
2025-05-07 22:02:10 +0300

    x

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { WalletProvider } from './context/WalletContext';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
@@ -16,25 +16,41 @@ import LitePaperPage from './pages/LitePaperPage';
 import QDayPage from './pages/QDayPage';
 import QuantumTheftPage from './pages/QuantumTheftPage';
 import Footer from './components/Footer';
+import { initGA, logPageView } from './lib/ga';
+
+function RouteChangeListener() {
+  const location = useLocation();
+
+  useEffect(() => {
+    logPageView(location.pathname);
+  }, [location]);
+
+  return null;
+}
 
 export default function App() {
+  useEffect(() => {
+    initGA();
+  }, []);
+
   return (
     <WalletProvider>
       <Router>
+        <RouteChangeListener />
         <div className="min-h-screen bg-slate-50 font-ubuntu">
           <Navbar />
           <Routes>
             <Route path="/" element={<HomePage />} />
-            {/*<Route path="/security" element={<SecurityPage />} /> 
-            <Route path="/docs" element={<DocsPage />} />*/}
+            {/* <Route path="/security" element={<SecurityPage />} /> */}
+            {/* <Route path="/docs" element={<DocsPage />} /> */}
             <Route path="/explorer" element={<ExplorerPage />} />
             <Route path="/news" element={<NewsPage />} />
-            {/*<Route path="/exchanges" element={<ExchangesPage />} />*/}
+            {/* <Route path="/exchanges" element={<ExchangesPage />} /> */}
             <Route path="/wallets" element={<WalletsPage />} />
             <Route path="/dashboard" element={<WalletDashboard />} />
-           {/* <Route path="/lite-paper" element={<LitePaperPage />} />*/}
-            {/*  <Route path="/q-day" element={<QDayPage />} />*/}
-           {/* <Route path="/quantum-theft" element={<QuantumTheftPage />} />*/}
+            {/* <Route path="/lite-paper" element={<LitePaperPage />} /> */}
+            {/* <Route path="/q-day" element={<QDayPage />} /> */}
+            {/* <Route path="/quantum-theft" element={<QuantumTheftPage />} /> */}
             <Route path="/faq" element={<FAQPage />} />
             <Route path="/team" element={<TeamPage />} />
           </Routes>
